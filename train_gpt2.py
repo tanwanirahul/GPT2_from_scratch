9e4d02b48819aca616a6a38f72830df8010b2c54
Rahul Tanwani <tanwanirahul@gmail.com>
2024-06-19 20:39:25 +0530

    Implementation of weight sharing between token embeddings (wte) and LM head (lm_head)

--- train_gpt2.py
+++ train_gpt2.py
@@ -134,6 +134,9 @@ class GPT(nn.Module):
                                         
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
     
+        # wte and lm_head layers share the weights. 
+        self.transformer.wte.weight = self.lm_head.weight
+
     def forward(self, batch_x, targets=None):
         '''
             Implements forward pass for the GPT model.
